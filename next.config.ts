8efcc198b2441b42654f6bf560184d174218155f
Ali Tech <alitech.pro@hotmail.com>
2025-11-23 20:14:31 +0500

    Final Play

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  onDemandEntries: {
+    // This stops Next.js from trying to prerender _not-found during build
+    maxInactiveAge: 1000 * 60 * 60,
+  },
+  // Or the nuclear option (don’t do this in production)
+  // eslint: { ignoreDuringBuilds: true },
+  // typescript: { ignoreBuildErrors: true },
 };
 
 export default nextConfig;
